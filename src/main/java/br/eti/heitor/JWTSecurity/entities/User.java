d9b9add40a13d479a3130cf7ca3d21b26789ded9
Heitor Pires de Souza <heitorpsouza22@gmail.com>
2026-04-29 11:25:20 -0300

    Ajustes de erros e começo de testes utilizando o Postman

--- src/main/java/br/eti/heitor/JWTSecurity/entities/User.java
+++ src/main/java/br/eti/heitor/JWTSecurity/entities/User.java
@@ -1,6 +1,7 @@
 package br.eti.heitor.JWTSecurity.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
  *
  * @author sesi3dia
  */
+@Entity
 public class User implements UserDetails {
 
     @Id
